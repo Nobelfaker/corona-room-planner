4f233c7693cfeb6611aefd8d603044448a27c867
GrongWorks <christian@grong.works>
2021-06-02 11:10:15 +0200

    added first version of code

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import {HashRouter as Router, NavLink, Route, Switch} from "react-router-dom";
+import Calendar from "./components/Calendar";
+import {Divider, Layout, Menu} from 'antd';
+import { PieChartOutlined } from '@ant-design/icons';
+import './App.css';
+import virusImg from "./images/virus.png";
+
+const { Header, Content, Sider } = Layout;
+const { SubMenu } = Menu;
+
+function App() {
+    const [collapsed, setCollapse] = useState(true);
+
+    return <React.Fragment>
+        <Layout style={{ minHeight: '100vh' }}>
+            <Router>
+                <Sider collapsible collapsed={collapsed} onCollapse={() => setCollapse(!collapsed)} width={250}>
+                    <div className="logo">
+                        <img src={virusImg} alt="VIRUS" />
+                    </div>
+                    <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                        <Menu.Item key="1" icon={<PieChartOutlined />}>
+                            <NavLink to={"/"}>Home</NavLink>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout className="site-layout">
+                    <Header className="site-layout-background" style={{ padding: 0 }} />
+                    <Content style={{ margin: '0 16px' }}>
+                        <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                             <Switch>
+                                 <Route path={"/"} exact component={Calendar}/>
+                             </Switch>
+                        </div>
+                    </Content>
+                </Layout>
+            </Router>
+        </Layout>
+    </React.Fragment>;
+}
+
+export default App;
---
+++ src/components/Calendar/index.js
@@ -0,0 +1,76 @@
+import React, {useEffect, useState} from "react";
+import { DatePicker, Divider, Button } from "antd";
+import moment from "moment";
+import axios from "axios";
+
+function Calendar () {
+    const [date, setDate] = useState(moment());
+    const [rooms, setRooms] = useState([]);
+
+    const maxLessons = 10;
+    const lessons = Array(maxLessons).fill(0);
+
+    useEffect(() => {
+        axios.get("http://localhost/corona-room-planner/backend/?task=get_rooms")
+            .then(result => {
+                setRooms(result.data);
+            })
+            .catch(error => {
+                console.log("ERROR", error);
+            })
+    }, []);
+
+    return <React.Fragment> 
+        <DatePicker 
+            defaultPickerValue={date}
+            defaultValue={date}
+            onChange={(date, dateString) => {
+                setDate(date);                
+            }} 
+        />
+        &nbsp;
+        <Button type="primary" onClick={() => {
+            window.alert("Ey, du Idi!");
+        }}>Raum buchen</Button>
+        <Divider />
+        <table style={{ width: "100%" }} border={1}>
+            <thead>
+                <tr>
+                    <th>Raumname</th>
+                    {
+                        lessons.map((item, index) => {
+                            return <th style={{ textAlign: "center" }}>{ index + 1 }</th>
+                        })
+                    }
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    rooms.map(room => {
+                        return <tr>
+                            <td>{room.name}</td>
+                            {
+                                lessons.map((lesson, index) => {
+                                    const lessonIndex = index + 1;
+                                    const dateString = date.format("YYYY-MM-DD");
+                                    if (room.bookings[dateString]) {
+                                        const lessonSearch = room.bookings[dateString].find(classItem => classItem.lessonNumber == lessonIndex)
+                                        if (!lessonSearch) {
+                                            return <td>&nbsp;</td>;
+                                        } else {
+                                            return <td style={{ backgroundColor: "#c00", textAlign: "center", fontWeight: "bold", color: "white" }}>{lessonSearch.className}</td>;
+                                        }
+                                    } else {
+                                        return <td>&nbsp;</td>;
+                                    }
+                                })
+                            }
+                        </tr>
+                    })
+                }
+            </tbody>
+        </table>
+    </React.Fragment>
+};
+
+export default Calendar;
